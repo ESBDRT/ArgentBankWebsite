5231f07bfde50d6a168598a12a366ae1c150f176
Esteban DROUET <Esteban.Drouet94400@gmail.com>
2024-06-19 22:52:02 +0200

    Added react router links

--- argentbank/src/components/header/header.jsx
+++ argentbank/src/components/header/header.jsx
@@ -6,22 +6,21 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-      <nav class="main-nav">
-      <a class="main-nav-logo" href="./index.html">
-        <img
+    <nav class="main-nav">
+      <Link to="/" className='main-nav-logo'>
+      <img
           class="main-nav-logo-image"
           src={argentBankLogo}
           alt="Argent Bank Logo"
         />
-        <h1 class="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
       <div>
-        <a class="main-nav-item" href="./sign-in.html">
+        <Link to="/login" className="main-nav-item">
           <FontAwesomeIcon icon={faUserCircle} />
           Sign In
-        </a>
+        </Link>
       </div>
-    </nav> 
+    </nav>
   )
 }
 
